e1afaee6ac567ae99616acd15b189456d0c6c918
tRICKSTER0707 <yadavamit200399@gmail.com>
2022-08-15 16:50:23 +0530

    created qr code files and added images

---
+++ qr_code.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img= qr.make("https://www.linkedin.com/in/amit-kumar-yadav-5b34ba227")
+img.save("linkdin_profile.png")
---
+++ qr_code02.py
@@ -0,0 +1,11 @@
+import qrcode
+from PIL import Image
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=20,
+                 border=5,)
+qr.add_data("https://www.linkedin.com/in/amit-kumar-yadav-5b34ba227")
+qr.make(fit=True)
+img=qr.make_image(fill_color="black",back_color="white")
+img.save("profile2.png")
